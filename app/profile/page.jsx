90b0ec31b84083cdf9aeae1b005882dbc8f9b152
Gareth Cottrell <gareth.cott@gmail.com>
2023-06-14 10:57:26 +0200

    Fix a data error/api error, add delete and edit button on prompt card

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,43 +1,44 @@
-"use client"
-import { useState, useEffect } from 'react'
-import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
+"use client";
 
-import Profile from '@components/Profile'
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/Profile";
 
 const MyProfile = () => {
-    const {data: session} = useSession();
+  const router = useRouter();
+  const { data: session } = useSession();
 
-    const [posts, setPosts] = useState([]);
+  const [myPosts, setMyPosts] = useState([]);
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-          const response = await fetch(`/api/users/${session?.user.id}/posts`);
-          const data = await response.json();
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setMyPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, [session?.user.id]);
+
+  const handleEdit = (post) => {
+  };
+
+  const handleDelete = async (post) => {
     
-          setPosts(data);
-        }
-    
-        if(session?.user.id) fetchPosts();
-        
-      }, []);
-    
-    const handleEdit = () => {
-
-    }
-
-    const handleDelete = () => {
-
-    }
+  };
 
   return (
     <Profile
-    name="My"
-    desc="Welcome to your profile page"
-    data={{posts}}
-    handleEdit={handleEdit}
-    handleDelete={handleDelete}/>
-  )
-}
+      name='My'
+      desc='Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination'
+      data={myPosts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
 
-export default MyProfile
+export default MyProfile;
