27f665d9d192037b61e7947ffb197f2f77ea9b7b
Gareth Cottrell <gareth.cott@gmail.com>
2023-06-14 08:22:02 +0200

    Add in catch error response

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -17,6 +17,6 @@ export const POST = async (req, res) => {
 
         return new Response(JSON.stringify(newPrompt), {status: 201})
     } catch (error) {
-        
+        return new Response("Failed to create a new prompt", {status: 500})
     }
 }
